19a3811d50d7b1ba6e08dfbc120bc2b121547bf6
restareaByWeezy <fjgkd7777@naver.com>
2023-05-07 23:32:29 +0900

    🧩 Type(type): migrating d3 to typescript

---
+++ src/features/BarChartRace/constants.ts
@@ -0,0 +1,38 @@
+type Data = {
+  name: string;
+  value: number;
+  color: string;
+};
+
+export const sampleData: Data[] = [
+  {
+    name: 'alpha',
+    value: 10,
+    color: '#f4efd3',
+  },
+  {
+    name: 'beta',
+    value: 15,
+    color: '#cccccc',
+  },
+  {
+    name: 'charlie',
+    value: 20,
+    color: '#c2b0c9',
+  },
+  {
+    name: 'delta',
+    value: 25,
+    color: '#9656a1',
+  },
+  {
+    name: 'echo',
+    value: 30,
+    color: '#fa697c',
+  },
+  {
+    name: 'foxtrot',
+    value: 35,
+    color: '#fcc169',
+  },
+];
--- src/features/BarChartRace/index.tsx
+++ src/features/BarChartRace/index.tsx
@@ -1,54 +1,106 @@
-import { csv, scaleBand, scaleLinear, select } from 'd3';
+import { max, scaleBand, scaleLinear, select, Selection } from 'd3';
 import React, { useEffect, useRef, useState } from 'react';
+
+import useInterval from '@/hooks/useInterval';
+
+import { sampleData } from './constants';
+
+const getRandomIndex = (array: any[]) => {
+  return Math.floor(array.length * Math.random());
+};
 
 type Data = {
   name: string;
-  date: string;
   value: number;
-  category: string;
+  color: string;
 };
 
 const BarChartRace = () => {
-  const [data, setData] = useState<Data[]>([]);
-  const svgRef = useRef<SVGSVGElement>(null);
+  const [iteration, setIteration] = useState(0);
+  const [start, setStart] = useState(false);
+  const [data, setData] = useState(sampleData);
 
-  const getData = async () => {
-    await csv('src/features/BarChartRace/data.csv').then((d) => {
-      const typedData = d as unknown as Data[];
-      setData(typedData);
-    });
-  };
-  useEffect(() => {
-    getData();
-  }, []);
-
-  useEffect(() => {
-    if (data) {
-      const svg = select(svgRef.current);
-
-      const xScale = scaleBand()
-        .domain(data.map((d) => d.name))
-        .range([0, 500])
-        .padding(0.5);
-
-      const yScale = scaleLinear()
-        .domain([0, Math.max(...data.map((d) => d.value))])
-        .range([300, 0]);
-
-      svg
-        .selectAll('.bar')
-        .data(data)
-        .join('rect')
-        .attr('class', 'bar')
-        .attr('x', (d) => xScale(d.name)!)
-        .attr('y', (d) => yScale(d.value))
-        .attr('width', xScale.bandwidth())
-        .attr('height', (d) => 300 - yScale(d.value));
+  useInterval(() => {
+    if (start) {
+      const randomIndex = getRandomIndex(data);
+      setData(
+        data.map((entry, index) =>
+          index === randomIndex ? { ...entry, value: entry.value + 10 } : entry,
+        ),
+      );
+      setIteration(iteration + 1);
     }
+  }, 500);
+
+  const svgRef = useRef<SVGSVGElement>(null);
+  const wrapperRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const svg = select(svgRef.current);
+
+    // sorting the data
+    data.sort((a, b) => b.value - a.value);
+
+    const yScale = scaleBand<number>()
+      .paddingInner(0.1)
+      .domain(data.map((value, index) => index))
+      .range([0, 200]);
+
+    const xScale = scaleLinear<number>()
+      .domain([0, max(data, (entry) => entry.value) as number])
+      .range([0, 400]);
+
+    // draw the bars
+    svg
+      .selectAll<SVGRectElement, Data>('.bar')
+      .data(data, (entry, index) => entry.name)
+      .join((enter) =>
+        enter
+          .append('rect')
+          .attr('y', (entry, index) => yScale(index) as number),
+      )
+      .attr('fill', (entry) => entry.color)
+      .attr('class', 'bar')
+      .attr('x', 0)
+      .attr('height', yScale.bandwidth())
+      .transition()
+      .attr('width', (entry) => xScale(entry.value))
+      .attr('y', (entry, index) => yScale(index) as number);
+
+    svg
+      .selectAll<SVGTextElement, Data>('.label')
+      .data(data, (entry, index) => entry.name)
+      .join((enter) =>
+        enter
+          .append('text')
+          .attr(
+            'y',
+            (entry, index) =>
+              (yScale(index) as number) + yScale.bandwidth() / 2 + 5,
+          ),
+      )
+      .text((entry) => `🐎 ... ${entry.name} (${entry.value} meters)`)
+      .attr('class', 'label')
+      .attr('x', 10)
+      .transition()
+      .attr(
+        'y',
+        (entry, index) =>
+          (yScale(index) as number) + yScale.bandwidth() / 2 + 5,
+      );
   }, [data]);
+
   return (
     <div>
-      <svg ref={svgRef} width={500} height={300}></svg>
+      <div
+        ref={wrapperRef}
+        style={{ width: '100%', height: '100%', marginBottom: '2rem' }}
+      >
+        <svg style={{ height: '100%' }} ref={svgRef}></svg>
+      </div>
+      <button onClick={() => setStart(!start)}>
+        {start ? 'Stop the race' : 'Start the race!'}
+      </button>
     </div>
   );
 };
